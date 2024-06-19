2e708b693ab4813526df711817d291a01edd2ac2
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2024-06-19 23:36:17 +0800

    uod

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
-const fs = require('fs');
-const https = require('https');
 const express = require('express');
 const app = express();
-
-// 使用自签名证书
-const options = {
-  key: fs.readFileSync('./server.key'),
-  cert: fs.readFileSync('./server.cert')
-};
-
-// 如果使用 Let’s Encrypt 获取的证书
-// const options = {
-//     key: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem'),
-//     cert: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/fullchain.pem')
-// };
 
 // 中间件函数：将所有请求重定向到 www.baidu.com，并带上路径和查询参数
 app.use((req, res, next) => {
@@ -21,7 +7,7 @@ app.use((req, res, next) => {
   res.redirect(targetUrl);
 });
 
-const port = 443;
-https.createServer(options, app).listen(port, () => {
-  console.log(`Server running at https://localhost:${port}/`);
+const port = 80;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
