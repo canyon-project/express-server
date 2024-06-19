2a057ac718cbb8afdfea36985f21f2e5f83cc5f2
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2024-06-19 23:28:46 +0800

    uod

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const app = express();
 
-// 中间件函数：将所有请求重定向到 www.baidu.com，并带上路径和查询参数
-app.use((req, res, next) => {
-    const targetUrl = `https://www.canyoncov.com${req.originalUrl}`;
-    res.redirect(targetUrl);
+// 使用自签名证书
+const options = {
+  key: fs.readFileSync('./server.key'),
+  cert: fs.readFileSync('./server.cert')
+};
+
+// 如果使用 Let’s Encrypt 获取的证书
+// const options = {
+//     key: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem'),
+//     cert: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/fullchain.pem')
+// };
+
+app.get('/', (req, res) => {
+  res.send('Hello, HTTPS!');
 });
 
-const port = 80;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+const port = 443;
+https.createServer(options, app).listen(port, () => {
+  console.log(`Server running at https://localhost:${port}/`);
 });
