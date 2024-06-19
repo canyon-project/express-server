6f7ad2d33a5b350b65e68e6c48a3228cc2112ccf
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2024-06-19 23:30:51 +0800

    uod

--- main.js
+++ main.js
@@ -15,8 +15,10 @@ const options = {
 //     cert: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/fullchain.pem')
 // };
 
-app.get('/', (req, res) => {
-  res.send('Hello, HTTPS!');
+// 中间件函数：将所有请求重定向到 www.baidu.com，并带上路径和查询参数
+app.use((req, res, next) => {
+  const targetUrl = `https://www.canyoncov.com${req.originalUrl}`;
+  res.redirect(targetUrl);
 });
 
 const port = 443;
